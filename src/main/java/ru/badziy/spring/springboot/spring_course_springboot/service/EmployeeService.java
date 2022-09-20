6c2078a097ab3366a3f1ac0af009e78baa015468
Badziev Alan <badjity@yandex.ru>
2022-09-20 11:25:41 +0300

    Initial commit

---
+++ src/main/java/ru/badziy/spring/springboot/spring_course_springboot/service/EmployeeService.java
@@ -0,0 +1,18 @@
+package ru.badziy.spring.springboot.spring_course_springboot.service;
+
+
+
+import ru.badziy.spring.springboot.spring_course_springboot.entity.Employee;
+
+import java.util.List;
+
+public interface EmployeeService {
+
+    public List<Employee> getAllEmployees();
+
+    public void saveEmployee(Employee employee);
+    public Employee getEmployee(int id);
+    public void deleteEmployee(int id);
+
+
+}
